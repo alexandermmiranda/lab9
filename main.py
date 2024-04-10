f8bcdb72a6ac188f46ba9a8d1bfe098f0a86bf9c
alexandermmiranda <alexmiranda228@gmail.com>
2024-04-10 17:14:33 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+def encode(password):
+    encoded_pass_list = []
+    encoded_pass = ''
+    for i in range(len(password)):
+        encoded_pass_list.append(int(password[i]) + 3)
+    for char in encoded_pass_list:
+        encoded_pass += str(char)
+    return encoded_pass
+def decode(encoded_pass):
+    decoded_pass_list = []
+    decoded_pass = ''
+    for i in range(len(encoded_pass)):
+        decoded_pass_list.append(int(encoded_pass[i]) - 3)
+    for char in decoded_pass_list:
+        decoded_pass += str(char)
+    return decoded_pass
+def print_menu():
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+def main():
+    global encoded_pass
+    while True:
+        print_menu()
+        selection = int(input("Please enter an option: "))
+        match selection:
+            case 1:
+                password = input("Please enter your password to encode: ")
+                encoded_pass = encode(password)
+                print("Your password has been encoded and stored!")
+            case 2:
+                decoded_pass = decode(encoded_pass)
+                print(f"The encoded password is {encoded_pass}, and the original password is {decoded_pass}.")
+            case 3:
+                break
+if __name__ == "__main__":
+    main()
